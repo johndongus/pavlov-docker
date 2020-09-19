e3892fe0e38e6e2388d08c52434750645166dbe5
John Dong <32533877+johndongus@users.noreply.github.com>
2020-09-19 08:59:07 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-
+#!/bin/sh
+STEAM_CLIENT_DLL=$( find ../ -name 'steamclient.so' -print -quit)
+UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
+mkdir -p "$HOME/.steam/sdk64"
+ln $STEAM_CLIENT_DLL "$HOME/.steam/sdk64/steamclient.so"
+ln $STEAM_CLIENT_DLL "$UE4_PROJECT_ROOT/Pavlov/Binaries/Linux/steamclient.so"
+chmod +x "$UE4_PROJECT_ROOT/Pavlov/Binaries/Linux/PavlovServer"
+"$UE4_PROJECT_ROOT/Pavlov/Binaries/Linux/PavlovServer" -PORT=7777 $@
